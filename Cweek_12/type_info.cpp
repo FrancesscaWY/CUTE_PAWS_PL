fd0d19bb5e30a31da96a66c4b1025fc188c6d103
FrancesscaWY <3449997118@qq.com>
2024-05-16 21:43:13 +0800

    page222

--- Cweek_12/type_info.cpp
+++ Cweek_12/type_info.cpp
@@ -7,7 +7,7 @@ int main(){
     int *i=reinterpret_cast<int*>(&a);//reinterpret_cast用于转换指针或引用的类型，但是不检查转换的安全性
     const string s="Hello,world!";
     string ss=const_cast<string&>(s);//const_cast用于去除指针或引用的const属性
-    //dynamic_cast用于转换指针或引用的类型，如果转换失败，则返回nullptr
+    //dynamic_cast 类指针 用于转换指针或引用的类型，如果转换失败，则返回nullptr
 //    dynamic_cast<string&>(s);,dynamic_cast只能用于多态类型的转换，而不能用于非多态类型的转换 ,所以这里会报错
     const type_info& ti1=typeid(a);
     const type_info& ti2=typeid(x);
