0f344c632ecdabf9f09a346673361495462a7a9c
FrancesscaWY <3449997118@qq.com>
2024-05-21 10:33:58 +0800

    kao_shi——ku

---
+++ Cweek_13/test_ksk.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int x=3;
+    if(x=5){
+        cout << ++x << endl;
+    }else{
+        cout << x << endl;
+    }
+    for(int i=0,j=0;!j&&i<=5;i++){
+        cout << i << endl;
+    }
+
+    return 0;
+}
+
+//
+// Created by 86138 on 2024/5/21.
+//
